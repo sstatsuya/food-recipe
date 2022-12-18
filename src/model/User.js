971c789a3ea14a7c12ab727eb3948e8e5805e3f3
LuongMinhTien <193111>
2022-12-18 20:11:54 +0700

    Protected user password

--- src/model/User.js
+++ src/model/User.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const User =  new Schema({
-    _id: {type: String, required: true},
-    name: {type: String},
-    username: {type: String},
-    password: {type: String},
-    token: {type: String},
-    level: {type: Number},
-})
-module.exports = mongoose.model('Users', User)
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const User = new Schema({
+  _id: { type: String, required: true },
+  name: { type: String },
+  username: { type: String },
+  password: { type: String, select: false },
+  token: { type: String },
+  level: { type: Number },
+});
+module.exports = mongoose.model("Users", User);
